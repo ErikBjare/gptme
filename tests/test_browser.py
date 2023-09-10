69f5b73631b6a85841fac831701262228966bd4a
Erik Bjäreholt <erik@bjareho.lt>
2023-09-10 13:50:34 +0200

    test: mark browser tests as slow

--- tests/test_browser.py
+++ tests/test_browser.py
@@ -1,11 +1,14 @@
+import pytest
 from gptme.tools.browser import load_page, search
 
 
+@pytest.mark.slow
 def test_browser():
     content = load_page("https://www.google.com/ncr?hl=en")
     print(content)
 
 
+@pytest.mark.slow
 def test_search():
     content = search("test")
     print(content)
