4f8407e0620279f9188b84a8c12a8741ca26d8c8
Erik Bjäreholt <erik@bjareho.lt>
2023-09-10 14:42:52 +0200

    test: added test for shell multiline output

--- tests/test_shell.py
+++ tests/test_shell.py
@@ -1,34 +1,40 @@
+from typing import Generator
+
+import pytest
 from gptme.tools.shell import ShellSession
 
 
-def test_echo():
-    # Initialize a shell session
+@pytest.fixture
+def shell() -> Generator[ShellSession, None, None]:
     shell = ShellSession()
+    yield shell
+    shell.close()
 
+
+def test_echo(shell):
     # Run a echo command
     ret, out, err = shell.run_command("echo 'Hello World!'")
     assert err.strip() == ""  # Expecting no stderr
     assert out.strip() == "Hello World!"  # Expecting stdout to be "Hello World!"
-    assert ret == 0  # Process should exit with 0
-
-    # Close the shell session
-    shell.close()
+    assert ret == 0
 
 
-def test_cd():
-    # Initialize a shell session
-    shell = ShellSession()
+def test_echo_multiline(shell):
+    # Run a echo command
+    ret, out, err = shell.run_command("echo 'Line 1\nLine 2'")
+    assert err.strip() == ""  # Expecting no stderr
+    assert out.strip() == "Line 1\nLine 2"  # Expecting stdout to be "Line 1\nLine 2"
+    assert ret == 0
 
+
+def test_cd(shell):
     # Run a cd command
     ret, out, err = shell.run_command("cd /tmp")
     assert err.strip() == ""  # Expecting no stderr
-    assert ret == 0  # Process should exit with 0
+    assert ret == 0
 
     # Check the current directory
     ret, out, err = shell.run_command("pwd")
     assert err.strip() == ""  # Expecting no stderr
     assert out.strip() == "/tmp"  # Should be in /tmp now
-    assert ret == 0  # Process should exit with 0
-
-    # Close the shell session
-    shell.close()
+    assert ret == 0
