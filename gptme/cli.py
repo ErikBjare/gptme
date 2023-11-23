8c1eadabf4a4ae5c36c54ce7bb94e74b3beebfc8
Erik Bjäreholt <erik@bjareho.lt>
2023-11-23 11:50:10 +0100

    fix: fixed a spelling error

--- gptme/cli.py
+++ gptme/cli.py
@@ -35,7 +35,7 @@ logger = logging.getLogger(__name__)
 print_builtin = __builtins__["print"]  # type: ignore
 
 LLMChoice = Literal["openai", "local"]
-ModelChoice = Literal["gpt-3.5-turbo", "gpt4"]
+ModelChoice = Literal["gpt-3.5-turbo", "gpt-4"]
 
 
 script_path = Path(os.path.realpath(__file__))
