ced202550fe44ef6db593a1745fc9a4ae9af42f1
Erik Bjäreholt <erik@bjareho.lt>
2024-03-14 21:17:37 +0100

    refactor: refactored eval/main.py into seperate files

--- eval/main.py
+++ eval/main.py
@@ -4,198 +4,22 @@ Evals for code generation tools.
 Inspired by a document by Anton Osika and Axel Theorell.
 """
 
-import base64
 import inspect
 import logging
-import os
-import subprocess
 import sys
-import tempfile
 import time
-from abc import abstractmethod
-from dataclasses import dataclass
-from pathlib import Path
-from typing import Callable, Dict, TypedDict
 
-from gptme.cli import chat as gptme_chat
-from gptme.message import Message
-from gptme.prompts import get_prompt
-
-from evals import tests, tests_map
+from .agents import GPTMe
+from .evals import tests, tests_map
+from .execenv import SimpleExecutionEnv
+from .types import (
+    CaseResult,
+    ExecResult,
+    ExecTest,
+    ResultContext,
+)
 
 logger = logging.getLogger(__name__)
-
-Files = Dict[str, str | bytes]
-
-
-class ExecutionEnv:
-    @abstractmethod
-    def run(self, command: str):
-        """
-        Runs a command in the execution environment.
-        """
-        raise NotImplementedError
-
-    @abstractmethod
-    def upload(self, files: Files):
-        """
-        Uploads files to the execution environment.
-        """
-        raise NotImplementedError
-
-    @abstractmethod
-    def download(self) -> Files:
-        """
-        Downloads files from the execution environment.
-        """
-        raise NotImplementedError
-
-
-@dataclass
-class ResultContext:
-    """
-    Context for the result of a test.
-    """
-
-    files: Files
-    stdout: str
-    stderr: str
-    exit_code: int
-
-
-class CaseResult(TypedDict):
-    name: str
-    passed: bool
-    code: str
-    duration: float
-
-
-class ExecResult(TypedDict):
-    name: str
-    results: list[CaseResult]
-    timings: dict[str, float]
-
-
-class ExecTest(TypedDict):
-    name: str
-    files: Files
-    run: str
-    prompt: str
-    expect: dict[str, Callable[[ResultContext], bool]]
-
-
-class FileStore:
-    def __init__(self):
-        self.working_dir = Path(tempfile.mkdtemp(prefix="gptme-evals-"))
-        self.working_dir.mkdir(parents=True, exist_ok=True)
-        self.id = self.working_dir.name.split("-")[-1]
-
-    def upload(self, files: Files):
-        for name, content in files.items():
-            path = self.working_dir / name
-            path.parent.mkdir(parents=True, exist_ok=True)
-            if isinstance(content, str):
-                with open(path, "w") as f:
-                    f.write(content)
-            elif isinstance(content, bytes):
-                with open(path, "wb") as f:
-                    f.write(base64.b64decode(content))
-
-    def download(self) -> Files:
-        files: Files = {}
-        for path in self.working_dir.glob("**/*"):
-            if path.is_file():
-                key = str(path.relative_to(self.working_dir))
-                try:
-                    with open(path, "r") as f:
-                        files[key] = f.read()
-                except UnicodeDecodeError:
-                    # file is binary
-                    with open(path, "rb") as f:
-                        files[key] = base64.b64encode(f.read())
-        return files
-
-
-class Agent:
-    @abstractmethod
-    def act(self, files: Files | None, prompt: str) -> Files:
-        """
-        Carries out the prompt and returns artifacts in the form of `Files`.
-        """
-        raise NotImplementedError
-
-
-class GPTMe(Agent):
-    def act(self, files: Files | None, prompt: str):
-        store = FileStore()
-        os.chdir(store.working_dir)  # can now modify store content
-
-        if files:
-            store.upload(files)
-
-        print("\n--- Start of generation ---")
-        print(f"Working in {store.working_dir}")
-        # TODO: add timeout
-        try:
-            gptme_chat(
-                [Message("user", prompt)],
-                [get_prompt()],
-                f"gptme-evals-{store.id}",
-                "openai",
-                "gpt-4-1106-preview",
-                no_confirm=True,
-                interactive=False,
-            )
-        # don't exit on sys.exit()
-        except (SystemExit, KeyboardInterrupt):
-            pass
-        print("--- Finished generation ---\n")
-
-        return store.download()
-
-
-class SimpleExecutionEnv(FileStore, ExecutionEnv):
-    """
-    A simple execution environment that runs the code in the files.
-
-    upload() and download() are inherited from FileStore.
-    """
-
-    def run(self, command) -> tuple[str, str, int]:
-        os.chdir(self.working_dir)
-
-        start = time.time()
-        print("\n--- Start of run ---")
-        # while running, also print the stdout and stderr
-        p = subprocess.Popen(
-            command,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-            cwd=self.working_dir,
-            text=True,
-            shell=True,
-        )
-        print("$", command)
-        stdout_full, stderr_full = "", ""
-        while p.poll() is None or p.stdout or p.stderr:
-            assert p.stdout is not None
-            assert p.stderr is not None
-            stdout = p.stdout.readline()
-            stderr = p.stderr.readline()
-            if stdout:
-                print(stdout, end="")
-                stdout_full += stdout
-            if stderr:
-                print(stderr, end="")
-                stderr_full += stderr
-            if not stdout and not stderr and p.poll() is not None:
-                break
-            if time.time() - start > 30:
-                print("Timeout!")
-                p.kill()
-                break
-        print("--- Finished run ---\n")
-        return stdout_full, stderr_full, p.returncode
 
 
 def execute(test: ExecTest) -> ExecResult:
--- gptme/__init__.py
+++ gptme/__init__.py
@@ -1,5 +1,6 @@
-from .cli import main
+from .cli import chat, main
 from .logmanager import LogManager
 from .message import Message
+from .prompts import get_prompt
 
-__all__ = ["main", "LogManager", "Message"]
+__all__ = ["main", "chat", "LogManager", "Message", "get_prompt"]
