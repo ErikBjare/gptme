38eadcb58c8f7e4f42e457664bd3e9896e26237d
Erik Bjäreholt <erik@bjareho.lt>
2024-09-09 18:38:52 +0200

    more fixes

--- gptme/tools/read.py
+++ gptme/tools/read.py
@@ -1,3 +1,7 @@
+"""
+Read the contents of a file.
+"""
+
 from gptme.tools.base import ToolSpec
 
 # Note: this isn't actually a tool, it only serves prompting purposes
@@ -9,3 +13,4 @@ tool = ToolSpec(
 cat file.txt
 ```""",
 )
+__doc__ = tool.get_doc(__doc__)
--- gptme/tools/save.py
+++ gptme/tools/save.py
@@ -1,16 +1,5 @@
 """
 Gives the assistant the ability to save code to a file.
-
-Example:
-
-.. chat::
-
-    User: write hello world to hello.py
-    Assistant:
-    ```save hello.py
-    print("hello world")
-    ```
-    System: Saved to hello.py
 """
 
 from collections.abc import Generator
@@ -138,6 +127,7 @@ tool_save = ToolSpec(
     execute=execute_save,
     block_types=["save"],
 )
+__doc__ = tool_save.get_doc(__doc__)
 
 instructions_append = """
 To append code to a file, use a code block with the language: append <filepath>
@@ -160,3 +150,4 @@ tool_append = ToolSpec(
     execute=execute_append,
     block_types=["append"],
 )
+__doc__ = tool_append.get_doc(__doc__)
