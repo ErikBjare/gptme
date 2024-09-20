abcfec0a9b8e0e8c7436eefd14076b46167ae4bb
Erik Bjäreholt <erik@bjareho.lt>
2024-09-20 12:47:08 +0200

    fix: set gptme.__version__

--- gptme/__init__.py
+++ gptme/__init__.py
@@ -1,3 +1,4 @@
+from .__version__ import __version__
 from .cli import chat, main
 from .codeblock import Codeblock
 from .logmanager import LogManager
@@ -5,3 +6,4 @@ from .message import Message
 from .prompts import get_prompt
 
 __all__ = ["main", "chat", "LogManager", "Message", "get_prompt", "Codeblock"]
+__version__ = __version__
