cafed4912e079e26262fc06141f6446b009c8d5a
Erik Bjäreholt <erik@bjareho.lt>
2024-10-06 15:19:42 +0200

    refactor: extracted `chat` and `step` from cli.py into new chat.py

---
+++ gptme/interrupt.py
@@ -0,0 +1,43 @@
+"""
+Sets up a KeyboardInterrupt handler to handle Ctrl-C during the chat loop.
+"""
+
+import time
+
+from .util import console
+
+interruptible = False
+last_interrupt_time = 0.0
+
+
+def handle_keyboard_interrupt(signum, frame):  # pragma: no cover
+    """
+    This handler allows interruption of the assistant or tool execution when in an interruptible state,
+    while still providing a safeguard against accidental exits during user input.
+    """
+    global last_interrupt_time
+    current_time = time.time()
+
+    if interruptible:
+        raise KeyboardInterrupt
+
+    # if current_time - last_interrupt_time <= timeout:
+    #     console.log("Second interrupt received, exiting...")
+    #     sys.exit(0)
+
+    last_interrupt_time = current_time
+    console.print()
+    # console.log(
+    #     f"Interrupt received. Press Ctrl-C again within {timeout} seconds to exit."
+    # )
+    console.log("Interrupted. Press Ctrl-D to exit.")
+
+
+def set_interruptible():
+    global interruptible
+    interruptible = True
+
+
+def clear_interruptible():
+    global interruptible
+    interruptible = False
--- tests/test_server.py
+++ tests/test_server.py
@@ -8,7 +8,7 @@ flask = pytest.importorskip(
 
 # noreorder
 from flask.testing import FlaskClient  # fmt: skip
-from gptme.cli import init  # fmt: skip
+from gptme.init import init  # fmt: skip
 from gptme.models import get_model  # fmt: skip
 from gptme.server.api import create_app  # fmt: skip
 
