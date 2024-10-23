f7b693d9a3e6177660c0cb8dba639c3b30ab1055
Erik Bjäreholt <erik@bjareho.lt>
2024-10-23 18:42:25 +0200

    fix: better error if attempting to run on Windows, refactored readline stuff into tabcomplete file

--- gptme/tabcomplete.py
+++ gptme/tabcomplete.py
@@ -1,11 +1,51 @@
+import atexit
 import logging
-import readline
 from functools import lru_cache
 from pathlib import Path
 
 from .commands import COMMANDS
+from .dirs import get_readline_history_file
+
+# noreorder
+try:
+    import readline  # fmt: skip
+except ImportError:  # pragma: no cover
+    raise Exception(
+        "Unsupported platform: readline not available.\nIf you are on Windows, use WSL or Docker to run gptme."
+    ) from None
+
 
 logger = logging.getLogger(__name__)
+
+
+# default history if none found
+# NOTE: there are also good examples in the integration tests
+history_examples = [
+    "What is love?",
+    "Have you heard about an open-source app called ActivityWatch?",
+    "Explain 'Attention is All You Need' in the style of Andrej Karpathy.",
+    "Explain how public-key cryptography works as if I'm five.",
+    "Write a Python script that prints the first 100 prime numbers.",
+    "Find all TODOs in the current git project",
+]
+
+
+def load_readline_history() -> None:  # pragma: no cover
+    logger.debug("Loading history")
+    # enabled by default in CPython, make it explicit
+    readline.set_auto_history(True)
+    # had some bugs where it grew to gigs, which should be fixed, but still good precaution
+    readline.set_history_length(100)
+    history_file = get_readline_history_file()
+    try:
+        readline.read_history_file(history_file)
+    except FileNotFoundError:
+        for line in history_examples:
+            readline.add_history(line)
+    except Exception:
+        logger.exception("Failed to load history file")
+
+    atexit.register(readline.write_history_file, history_file)
 
 
 def register_tabcomplete() -> None:  # pragma: no cover
