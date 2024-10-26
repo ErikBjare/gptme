56bd22f93bab801d8f2d794a72f36db250623fcd
Brayo <vukubrian@gmail.com>
2024-10-26 17:26:31 +0300

    feat: compile regex and improve readability

--- gptme/prompts.py
+++ gptme/prompts.py
@@ -230,16 +230,16 @@ def prompt_systeminfo() -> Generator[Message, None, None]:
 
     yield Message( "system", prompt,)
 
+regex = re.compile(r"^NAME=\"?([^\"]+)\"?")
 def get_system_distro() -> str:
     """Get the system distribution name."""
-    regex = re.compile(r"^NAME=\"?([^\"]+)\"?")
 
     if os.path.exists("/etc/os-release"):
         with open("/etc/os-release") as f:
             for line in f:
-                matches = re.search(regex, line)
-                if matches:
-                    return matches.string[matches.start(1) : matches.end(1)]
+                match = regex.match(line)
+                if match:
+                    return match.group(1)
     return "Linux"
 
 document_prompt_function(interactive=True)(prompt_gptme)
