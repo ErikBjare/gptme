b3d378d932eb9f02db15ec99589e0d3e14638322
Erik Bjäreholt <erik@bjareho.lt>
2024-10-28 11:55:23 +0100

    fix: fixed browser test

--- tests/test_browser.py
+++ tests/test_browser.py
@@ -1,19 +1,10 @@
 import pytest
 
+# TODO: we should also test lynx backend
 playwright = pytest.importorskip("playwright")
 
 # noreorder
-from gptme.tools.browser import load_page, read_url, search  # fmt: skip
-
-# noreorder
-from playwright.sync_api import expect  # fmt: skip
-
-
-@pytest.mark.slow
-def test_browser():
-    page = load_page("https://superuserlabs.org")
-    expect(page.get_by_role("main")).to_contain_text("Erik Bjäreholt")
-
+from gptme.tools.browser import read_url, search  # fmt: skip
 
 # FIXME: Broken
 # @pytest.mark.slow
@@ -37,5 +28,5 @@ def test_read_url_with_links():
     # check that "Erik Bjäreholt" is present
     assert "Erik Bjäreholt" in s
 
-    # check that markdown link to activitywatch is present
-    assert "(https://activitywatch.net/)" in s
+    # check that link to activitywatch is present
+    assert "https://activitywatch.net/" in s
