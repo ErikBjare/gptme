f8c296f5196fa76832e1a23f06a540617179a6a8
Erik Bjäreholt <erik@bjareho.lt>
2024-10-30 14:43:58 +0100

    fix: fixed __version__ looking up old package name

--- gptme/__version__.py
+++ gptme/__version__.py
@@ -1,6 +1,9 @@
 import importlib.metadata
 
 try:
-    __version__ = importlib.metadata.version("gptme-pythom")
+    __version__ = importlib.metadata.version("gptme")
 except importlib.metadata.PackageNotFoundError:
     __version__ = "0.0.0 (unknown)"
+
+if __name__ == "__main__":
+    print(__version__)
