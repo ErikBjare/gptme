2e71f5ffd1b0ac5639ef3f8416a67125dd3d6151
Erik Bjäreholt <erik@bjareho.lt>
2025-02-28 10:20:03 +0100

    tests: fixed test

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,5 +1,6 @@
 import os
 import random
+import tempfile
 from pathlib import Path
 from tempfile import TemporaryDirectory
 
@@ -205,9 +206,10 @@ def test_shell_file(args: list[str], runner: CliRunner):
     # test running the shell tool with a filename
     # make sure we don't accidentally expand the filename and include it in the shell command
     # create new file with contents
-    with open("test.txt", "w") as f:
+    tmp_path = tempfile.mktemp()
+    with open(tmp_path, "w") as f:
         f.write("yes")
-    args.append("/shell cat test.txt")
+    args.append(f"/shell cat {tmp_path}")
     result = runner.invoke(gptme.cli.main, args)
     output_pre, output_post = result.output.split("System", 1)
     # check for no 'yes' in parsed input (only direct command output)
